9317360c3ae50b4ee974af0a5ce822b409346bf5
anant11111 <127534504+anant11111@users.noreply.github.com>
2025-11-17 16:53:34 +0000

    adding changes

--- dags/spark_etl_dag.py
+++ dags/spark_etl_dag.py
@@ -4,7 +4,6 @@ from airflow.sensors.filesystem import FileSensor
 from airflow.operators.bash import BashOperator
 import os
 
-# Base directories (local paths in Codespaces)
 BASE_PATH = "/workspaces/spark-airflow-etl"
 INPUT_PATH = f"{BASE_PATH}/data/input/events.parquet"
 OUTPUT_PATH = f"{BASE_PATH}/data/output/"
@@ -21,28 +20,26 @@ with DAG(
     dag_id="spark_daily_etl",
     default_args=default_args,
     start_date=datetime(2024, 1, 1),
-    schedule="0 0 * * *",  # Daily at midnight
+    schedule="0 0 * * *",
     catchup=False,
 ) as dag:
 
-    # Sensor waits for input data to appear
     wait_for_data = FileSensor(
         task_id="wait_for_data",
         filepath="data/input/events.parquet",
         fs_conn_id="fs_default",
         poke_interval=30,
-        timeout=60 * 60,
+        timeout=3600,
         mode="poke",
     )
 
-    # Run Spark job
     run_spark_etl = BashOperator(
         task_id="run_spark_etl",
         bash_command=f"""
         python {BASE_PATH}/spark_job/app.py \
             --input-path {INPUT_PATH} \
             --output-path {OUTPUT_PATH} \
-            --date {{ ds }}
+            --date {{{{ ds }}}}
         """,
     )
 
